c008f24fc27e7d53de7b12c86a4d0c7981eb7b11
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-12-07 14:27:09 +0900

    CommonViewModel追加、 ComposeBasedFragmentでの使用例

---
+++ app/src/main/java/org/nunocky/mutuallyusableviewmodelsample/CommonViewModel.kt
@@ -0,0 +1,59 @@
+package org.nunocky.mutuallyusableviewmodelsample
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+suspend fun taskXFunction(count: Int): String {
+    delay(1500)
+
+    if (count % 3 == 0) {
+        throw Exception("error")
+    }
+
+    return "Hello"
+}
+
+sealed class TaskXUiState {
+    data object Idle : TaskXUiState()
+    data object Loading : TaskXUiState()
+    data class Success(val text: String) : TaskXUiState()
+    data class Error(val message: String) : TaskXUiState()
+}
+
+class CommonViewModel : ViewModel() {
+    private val _count = MutableStateFlow(0)
+    val count = _count.asStateFlow()
+
+    private val _uiState = MutableStateFlow<TaskXUiState>(TaskXUiState.Idle)
+    val uiState = _uiState.asStateFlow()
+
+    private var _shouldShowDialog = MutableStateFlow(false)
+    val shouldShowDialog = _shouldShowDialog.asStateFlow()
+
+    fun increment() {
+        _count.value++
+    }
+
+    fun processASyncFunction() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _uiState.value = TaskXUiState.Loading
+            runCatching {
+                taskXFunction(count.value)
+            }.onSuccess { text ->
+                _uiState.value = TaskXUiState.Success(text)
+            }.onFailure {
+                _shouldShowDialog.value = true
+                _uiState.value = TaskXUiState.Error(it.message ?: "unknown error")
+            }
+        }
+    }
+
+    fun onDialogDismissed() {
+        _shouldShowDialog.value = false
+    }
+}
--- app/src/main/java/org/nunocky/mutuallyusableviewmodelsample/ui/ComposeBasedFragment.kt
+++ app/src/main/java/org/nunocky/mutuallyusableviewmodelsample/ui/ComposeBasedFragment.kt
@@ -4,11 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import org.nunocky.mutuallyusableviewmodelsample.CommonViewModel
+import org.nunocky.mutuallyusableviewmodelsample.TaskXUiState
 
 class ComposeBasedFragment : Fragment() {
+    private val viewModel: CommonViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,7 +40,78 @@ class ComposeBasedFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text("Hello")
+                ComposesBasedScreen(viewModel)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ComposesBasedScreen(viewModel: CommonViewModel = viewModel(), modifier: Modifier = Modifier) {
+
+    val count by viewModel.count.collectAsStateWithLifecycle()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val shouldShowDialog by viewModel.shouldShowDialog.collectAsStateWithLifecycle()
+
+    Column(modifier = modifier) {
+        Text("Count: $count")
+        Button(onClick = { viewModel.increment() }) {
+            Text("Increment")
+        }
+
+        when (val state = uiState) {
+            TaskXUiState.Idle -> {
+                Button(onClick = { viewModel.processASyncFunction() }) {
+                    Text("Click")
+                }
+            }
+
+            TaskXUiState.Loading -> {
+                Text("loading...")
+            }
+
+            is TaskXUiState.Success -> {
+                Column {
+                    Text(state.text)
+                    Button(onClick = { viewModel.processASyncFunction() }) {
+                        Text("Click")
+                    }
+                }
+            }
+
+            is TaskXUiState.Error -> {
+                Text(state.message)
+                Button(onClick = { viewModel.processASyncFunction() }) {
+                    Text("Click")
+                }
+            }
+        }
+    }
+
+    if (shouldShowDialog) {
+        BasicAlertDialog(onDismissRequest = {
+            viewModel.onDialogDismissed()
+        }) {
+            Surface(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large,
+                tonalElevation = AlertDialogDefaults.TonalElevation
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "something went wrong")
+                    Spacer(modifier = Modifier.height(24.dp))
+                    TextButton(
+                        onClick = {
+                            viewModel.onDialogDismissed()
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Confirm")
+                    }
+                }
             }
         }
     }
